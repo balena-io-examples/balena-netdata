18ad7828275058e99001f61091ef589e3f628333
Hugh Brown <hugh@balena.io>
2020-06-22 15:11:39 -0700

    Add upstream wrapper file to handle balena-engine permissions
    - Add upstream file, which sets group membership correctly for netdata
    user
    - Add note about license; remove polyverse download
    - Add shadow package needed for group membership tools
    
    Change-type: [patch|minor|major]
    Signed-off-by: Hugh Brown <hugh@balena.io>

---
+++ netdata/upstream-docker-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Entry point script for netdata
+#
+# Copyright: SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Author  : Pavlos Emm. Katsoulakis <paul@netdata.cloud>
+#
+# BALENA_NOTES:
+# - Upstream source: https://github.com/netdata/netdata/commit/9511af410de5e47f9e1fd7a0718dd98d82d6edb4
+# - Removed Polymorphic Linux download
+
+set -e
+
+if [ ! "${DO_NOT_TRACK:-0}" -eq 0 ] || [ -n "$DO_NOT_TRACK" ]; then
+  touch /etc/netdata/.opt-out-from-anonymous-statistics
+fi
+
+if [ -n "${PGID}" ]; then
+  echo "Creating docker group ${PGID}"
+  addgroup -g "${PGID}" "docker" || echo >&2 "Could not add group docker with ID ${PGID}, its already there probably"
+  echo "Assign netdata user to docker group ${PGID}"
+  usermod -a -G "${PGID}" "${DOCKER_USR}" || echo >&2 "Could not add netdata user to group docker with ID ${PGID}"
+fi
+
+# See https://github.com/netdata/netdata/issues/4173
+# exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" -W set web "web files group" root -W set web "web files owner" root "$@"
+exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" "$@"
+
+echo "Netdata entrypoint script, completed!"
